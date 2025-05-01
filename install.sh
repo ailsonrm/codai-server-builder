cadba1c2c0ffdd27f92f41e8fb21a2ef496adb73
Ailson Ramos Moreira <ailson.rmoreira@gmail.com>
2025-05-01 10:44:41 -0300

    update

--- install.sh
+++ install.sh
@@ -268,9 +268,9 @@ else
 fi
 
 # Initialize AIDE
-#print_message "${YELLOW}" "Initialize AIDE..."
-#aide --config=/etc/aide/aide.conf --init
-#mv /var/lib/aide/aide.db.new /var/lib/aide/aide.db
+print_message "${YELLOW}" "Initialize AIDE..."
+ide --config=/etc/aide/aide.conf --init
+v /var/lib/aide/aide.db.new /var/lib/aide/aide.db
 
 # Configure kernel parameters
 print_message "${YELLOW}" "Configure kernel parameters..."
